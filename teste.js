c0eeaea3913bdb3f90a9bd5ee925f9a9bcb7d353
Leonardo Santos Silva <1532leleo@gmail.com>
2022-11-21 22:15:44 -0300

    Lista 29 e 29.2 feita

--- teste.js
+++ teste.js
@@ -1,33 +1,23 @@
 // De acordo com o diagrama de classes acima, crie as respectivas classes, todas devem ter
 // um método construtor respeitando as seguintes indicações:
-
 // 1. A propriedade Titular da classe PacoteViagem deve receber seu valor através da
 // propriedade Passageiro da classe PassagemArea;
-
 // 2. O método CalcularValor() da classe PassagemArea deve fazer um acréscimo de
 // 50% no valor base caso a propriedade PrimeiraClasse seja “true”
-
 // 3. O método ExibirResumo da classe PassagemArea deve exibir uma mensagem com
 // as seguintes informações: “Passagem em nome de {nome do passageiro}, no
 // assento {assento} do voo {número do voo}, com destino para {local de destino do
 // voo}!”
 
-
 // 1. Encapsular as propriedades de todas as classes e criar seus respectivos métodos
 // get/set (para definição e leitura dos valores dessa propriedade));
-
 // 2. Na classe Cliente criar as seguintes validações no método set das respectivas
 // propriedades:
-
 // a. Nome deve conter no máximo 50 caracteres;
-
 // b. Cpf deve conter no máximo 11 caracteres;
-
 // 3. Na classe PassagemArea criar as seguintes validações no método set das
 // respectivas propriedades:
-
 // a. PrimeiraClasse deve aceitar somente um valor booleano;
-
 // b. Valor deve ser sempre maior do que zero;
 
 class Cliente {
@@ -40,14 +30,33 @@ class Cliente {
         this.setCpf(cpf)
         this.setDataNascimento(dataNascimento)
     }
+
     setNome(nome) {
-        this.#Nome = nome
+        let continuar = true
+        while (continuar) {
+            if (nome.length > 50) {
+                console.log("O nome deve ter no máximo 50 caracteres!")
+                nome = prompt("Insira um novo nome")
+            } else {
+                this.#Nome = nome
+                continuar = false
+            }
+        }
     }
     getNome() {
         return this.#Nome
     }
     setCpf(cpf) {
-        this.#Cpf = cpf
+        let continuar2 = true
+        while (continuar2) {
+            if (cpf.length > 11) {
+                console.log("O cpf deve ter no máximo 11 caracteres!")
+                cpf = prompt("Insira um novo cpf")
+            } else {
+                this.#Cpf = cpf
+                continuar2 = false
+            }
+        }
     }
     getCpf() {
         return this.#Cpf
@@ -82,13 +91,26 @@ class PassagemAerea {
         return this.#Assento
     }
     setPrimeiraClasse(primeiraClasse) {
-        this.#PrimeiraClasse = primeiraClasse
+        if (primeiraClasse == "s") {
+            primeiraClasse = true
+            this.#PrimeiraClasse = primeiraClasse
+        } else if (primeiraClasse == "n")
+            primeiraClasse = false
     }
     getPrimeiraClasse() {
         return this.#PrimeiraClasse
     }
     setValor(valor) {
-        this.#Valor = valor
+        let continuar3 = true
+        while(continuar3){
+            if(valor <= 0){
+                console.log("O valor deve ser maior que zero (0)")
+                valor = prompt("Insira um valor corretamente")
+            }else{
+                this.#Valor = valor
+                continuar3 = false
+            }
+        }
     }
     getValor() {
         return this.#Valor
@@ -109,18 +131,18 @@ class PassagemAerea {
 
     CalcularValor() {
         let valorPrimeiraClasse = 0
-        if (this.PrimeiraClasse == true) {
-            valorPrimeiraClasse = this.Valor + (this.Valor * 0.5)
+        if (this.#PrimeiraClasse == true) {
+            valorPrimeiraClasse = this.#Valor + (this.#Valor * 0.5)
             console.log("Com o acréscimo da Primeira Classe o valor total é:     R$ " + valorPrimeiraClasse)
-            this.Valor = valorPrimeiraClasse
+            this.#Valor = valorPrimeiraClasse
         } else {
-            valorPrimeiraClasse = this.Valor
+            valorPrimeiraClasse = this.#Valor
             console.log("Sua passagem Aérea foi de classe econômica!")
         }
     }
     ExibirResumo(passagemAereaAssento, vooNumero, vooLocalDestino) {
 
-        console.log("Passagem em nome de: " + Cliente1.Nome)
+        console.log("Passagem em nome de: " + this.#Passageiro.Nome)
         console.log("No assento: " + passagemAereaAssento)
         console.log("Do voo: " + vooNumero)
         console.log("Com destino para: " + vooLocalDestino)
@@ -171,13 +193,13 @@ class Voo {
     setLocalPartida(localPartida) {
         this.#LocalPartida = localPartida
     }
-    getLocalPartida(){
+    getLocalPartida() {
         return this.#LocalPartida
     }
-    setLocalDestino(localDestino){
+    setLocalDestino(localDestino) {
         this.#LocalDestino = localDestino
     }
-    getLocalDestino(){
+    getLocalDestino() {
         return this.#LocalDestino
     }
 }
@@ -195,28 +217,28 @@ class PacoteViagem {
         this.setValorTotal(valorTotal)
     }
 
-    setTitular(titular){
+    setTitular(titular) {
         this.#Titular = titular
     }
-    getTitular(){
+    getTitular() {
         return this.#Titular
     }
-    setPassagemIda(passagemIda){
+    setPassagemIda(passagemIda) {
         this.#PassagemIda = passagemIda
     }
-    getPassagemIda(){
+    getPassagemIda() {
         return this.#PassagemIda
     }
-    setPassagemVolta(passagemVolta){
+    setPassagemVolta(passagemVolta) {
         this.#PassagemVolta = passagemVolta
     }
-    getPassagemVolta(){
+    getPassagemVolta() {
         return this.#PassagemVolta
     }
-    setValorTotal(valorTotal){
+    setValorTotal(valorTotal) {
         this.#ValorTotal = valorTotal
     }
-    getValorTotal(){
+    getValorTotal() {
         return this.#ValorTotal
     }
 }
