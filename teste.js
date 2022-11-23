3cc4fd903f76952aefa2dc52605a2ce3cbaf7a1e
Leonardo Santos Silva <1532leleo@gmail.com>
2022-11-22 22:17:22 -0300

    Lista 29 e 29.2 e 29.3 80%

--- teste.js
+++ teste.js
@@ -50,7 +50,8 @@
 
 let Clientes = []
 let Voos = []
-
+let PacotesDeViagens = []
+let PassagensAereas = []
 
 class Cliente {
     #Nome
@@ -315,34 +316,153 @@ while (continuarExecutando) {
             break;
         case "2":
             let empresa = prompt("Insira a Empresa")
+
             let numero = prompt("Insira o numero do voo")
+
             let data = prompt("Insira a data do voo")
-            let horario = prompt("Insira o horário do voo")
+
+            let horario = parseInt(prompt("Insira o horário do voo"))
+
             let localPartida = prompt("Insira o local de partida do voo")
+
             let localDestino = prompt("Insira o local de destino do voo")
-            let Voo1 = new Voo(empresa, numero, data, horario, localPartida, localDestino)
-            Voos.push(Voo1)
+
+            let voo1 = new Voo(empresa, numero, data, horario, localPartida, localDestino)
+            Voos.push(voo1)
+
+            let empresa2 = prompt("Insira a Empresa")
+
+            let numero2 = prompt("Insira o numero do voo")
+
+            let data2 = prompt("Insira a data do voo")
+
+            let horario2 = parseInt(prompt("Insira o horário do voo"))
+
+            let localPartida2 = prompt("Insira o local de partida do voo")
+
+            let localDestino2 = prompt("Insira o local de destino do voo")
+
+            let voo2 = new Voo(empresa2, numero2, data2, horario2, localPartida2, localDestino2)
+            Voos.push(voo2)
             break;
 
-            
         case "3":
 
-            break
+            // c. Na hora de escolher um assento na passagem aérea, verifique se esse
+            // assento já não foi escolhido nesse voo, em caso positivo peça para escolher
+            // outro assento;
+
+            console.log(Voos)
+
+            let VerificacaoDeAssento = true
+            while (VerificacaoDeAssento) {
+
+                let passagemIdaAssento = parseInt(prompt("Qual o assento da sua passagem de Ida?"))
+
+                for (let i = 0; i < PassagensAereas.length; i++) {
+
+                    if(passagemIdaAssento == PassagensAereas[i]){
+                        console.log("O assento ja foi escolhido, escolha outro!")
+
+                    }else{
+                        console.log("Assento Disponível!")
+                        VerificacaoDeAssento = false
+                    }
+                }
+            }
+
+            let passagemIdaPrimeiraClasse = prompt("Deseja ir de primeira classe? s ou n")
+
+            let passagemIdaValor = parseFloat(prompt("Insira o valor da passagem Aérea"))
+
+            let passageiroIda = prompt("Insira o nome do passageiro")
+            let passageiroPassagemIda
+            for (let index2 = 0; index2 < Clientes.length; index2++) {
+                if (passageiroIda == Clientes[index2].getNome()) {
+                    passageiroPassagemIda = Clientes[index2]
+                }
+            }
+            let passagemIdaVoo = prompt("Insira a data do voo")
+            let vooIda
+            for (let index3 = 0; index3 < Voos.length; index3++) {
+                if (passagemIdaVoo == Voos[index3].getData) {
+                    vooIda = Voos[index3]
+                }
+            }
+            let passagemIda = new PassagemAerea(passagemIdaAssento, passagemIdaPrimeiraClasse, passagemIdaValor, passageiroPassagemIda, vooIda)
+            PassagensAereas.push(passagemIda)
+
+            for (let contador = 0; contador < Voos.length; contador++) {
+                if (Voos[contador].getLocalPartida() == vooIda.getLocalDestino()) {
+                    console.log(Voos[contador])
+                }
+            }
+
+            let passagemVoltaAssento = parseInt(prompt("Qual o assento da sua passagem de Volta?"))
+
+            let passagemVoltaPrimeiraClasse = prompt("Deseja ir de primeira classe? s ou n")
+
+            let passagemVoltaValor = parseFloat(prompt("Insira o valor da passagem Aérea"))
+
+            let passageiroVolta = prompt("Insira o nome do passageiro")
+            let passageiroPassagemVolta
+            for (let index4 = 0; index4 < Clientes.length; index4++) {
+                if (passageiroVolta == Clientes[index4].getNome()) {
+                    passageiroPassagemVolta = Clientes[index4]
+                }
+            }
+            let passagemVoltaVoo = prompt("Insira a data do voo")
+            let vooVolta
+            for (let index5 = 0; index5 < Voos.length; index5++) {
+                if (passagemVoltaVoo == Voos[index5].getData) {
+                    vooVolta = Voos[index5]
+                }
+            }
+            let passagemVolta = new PassagemAerea(passagemVoltaAssento, passagemVoltaPrimeiraClasse, passagemVoltaValor, passageiroPassagemVolta, vooVolta)
+            PassagensAereas.push(passagemVolta)
+
+
+            let titular = prompt("Insira o nome do Titular")
+            let titularPacoteViagem
+            for (let index6 = 0; index6 < Clientes.length; index6++) {
+                if (titular == Clientes[index6].getNome()) {
+                    titularPacoteViagem = Clientes[index6]
+                }
+            }
+
+            let valorTotalPacote = passagemIdaValor + passagemVoltaValor
+
+            let pacoteViagem1 = new PacoteViagem(titularPacoteViagem, passagemIda, passagemVolta, valorTotalPacote)
+            PacotesDeViagens.push(pacoteViagem1)
+            break;
+
+
     }
     let desejaContinuar = prompt("Deseja continuar executando? s ou n")
     if (desejaContinuar != "s") {
         continuarExecutando = false
     }
 }
+// a. Quando for cadastrar um pacote de viagem, na hora de escolher a
+// passagem de Ida mostra uma lista com todos os Voo cadastrados;
+
+// b. Na hora de cadastrar a passagem de Volta, exibir apenas os Voo que tem o
+// local de partida igual ao local de destino do Voo de Ida escolhido;
+
+// c. Na hora de escolher um assento na passagem aérea, verifique se esse
+// assento já não foi escolhido nesse voo, em caso positivo peça para escolher
+// outro assento;
+
+// Fique a vontade para criar funções para ajudar na execução / organização do seu
+// código!
 
 
+// let Voo1 = new Voo("Emirates", "427816728390", "21/12/2022", 15, "GRU Airport(São Paulo, Brasil)", "Aeroporto Internacional de Dubai(Al Garhoud, Dubai)")
 
-let Voo1 = new Voo("Emirates", "427816728390", "21/12/2022", 15, "GRU Airport(São Paulo, Brasil)", "Aeroporto Internacional de Dubai(Al Garhoud, Dubai)")
+// let Voo2 = new Voo("Emirates", "168203978283", "31/12/2022", 8, "Aeroporto Internacional de Dubai(Al Garhoud, Dubai)", "GRU Airport(São Paulo, Brasil)")
 
-let Voo2 = new Voo("Emirates", "168203978283", "31/12/2022", 8, "Aeroporto Internacional de Dubai(Al Garhoud, Dubai)", "GRU Airport(São Paulo, Brasil)")
+// let PassagemAerea1 = new PassagemAerea(28, true, 12000, Cliente1, Voo1)
 
-let PassagemAerea1 = new PassagemAerea(28, true, 12000, Cliente1, Voo1)
+// let PassagemAerea2 = new PassagemAerea(44, false, 12000, Cliente1, Voo2)
 
-let PassagemAerea2 = new PassagemAerea(44, false, 12000, Cliente1, Voo2)
-
-let PacoteViagem1 = new PacoteViagem(Cliente1, PassagemAerea1, PassagemAerea2, 30000)
+// let PacoteViagem1 = new PacoteViagem(Cliente1, PassagemAerea1, PassagemAerea2, 30000)
